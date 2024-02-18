4fe76da60597aebfd31981659a6bff2d77aff128
Vladyslav Plaksa <vladyslavplaksa@gmail.com>
2024-02-18 00:03:01 +0000

    feat: add Topnav template from NextUi exaples

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@nextui-org/button';
 
+import Topnav from '@/shared/Topnav';
+
 export default function Home() {
     return (
         <div className='h-44'>
+            <Topnav />
             <Button>Click me</Button>
         </div>
     );
