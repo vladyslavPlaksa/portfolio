277601679244d906d75d1d926a488629a5a95ceb
Vladyslav Plaksa <vladyslavplaksa@gmail.com>
2024-02-21 12:06:03 +0000

    refactor: change next.js file structure from app/ to page/

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { nextui } from '@nextui-org/react';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -6,7 +5,7 @@ const config: Config = {
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
         './components/**/*.{js,ts,jsx,tsx,mdx}',
         './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+        './shared/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         extend: {
@@ -16,7 +15,6 @@ const config: Config = {
             },
         },
     },
-    darkMode: 'class',
-    plugins: [nextui()],
+    plugins: [],
 };
 export default config;
