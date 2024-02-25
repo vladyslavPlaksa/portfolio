09259ce2b4b8b6b804db8e7228abbdcbb2b73069
Vladyslav Plaksa <vladyslavplaksa@gmail.com>
2024-02-25 11:55:20 +0000

    fix: delete unnecessary id

--- shared/Sections/HeroSection.tsx
+++ shared/Sections/HeroSection.tsx
@@ -6,9 +6,7 @@ export default function HeroSection() {
     return (
         <div className='px-3 pt-[150px] text-center sm:px-10 sm:text-right'>
             <h1 className='font-michroma text-[70px] sm:text-[120px] md:text-[160px] lg:text-[200px]'>{t(`title`)}</h1>
-            <p className='font-michroma text-xl md:text-[30px] md:leading-8 lg:text-[45px] lg:leading-[50px]' id='skills'>
-                {t(`subtitle`)}
-            </p>
+            <p className='font-michroma text-xl md:text-[30px] md:leading-8 lg:text-[45px] lg:leading-[50px]'>{t(`subtitle`)}</p>
         </div>
     );
 }
