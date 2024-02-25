87064079c439ec2458f2510479d1ae79332ced52
Vladyslav Plaksa <vladyslavplaksa@gmail.com>
2024-02-25 15:04:35 +0000

    refactor: make an option to customize styles

--- shared/utils/Button.tsx
+++ shared/utils/Button.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react';
 
-const Button = ({ children }: { children: ReactNode }) => {
+const Button = ({ children, className }: { children: ReactNode; className?: string }) => {
     return (
         <span
-            className={
-                'bg-secondary-green/25 border-secondary-green dark:bg-secondary-pink/25 dark:border-secondary-pink text-md hover:bg-secondary-green/65 dark:hover:bg-secondary-pink/65 border border-solid px-3 py-1 font-michroma transition duration-300 ease-in-out sm:text-lg md:text-xl'
-            }
+            className={`bg-secondary-green/25 border-secondary-green dark:bg-secondary-pink/25 dark:border-secondary-pink  hover:bg-secondary-green/65 dark:hover:bg-secondary-pink/65 cursor-default border border-solid px-3 py-1 font-michroma transition duration-300 ease-in-out ${className ? `${className}` : 'text-md sm:text-lg md:text-xl'}`}
         >
             {children}
         </span>
